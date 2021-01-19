02d0dabd955659c974a1a4650b99bba1aea47e9b
Alessandro <asovitorio@gmail.com>
2021-01-18 22:44:27 -0300

    :bento: test
    test

--- src/api/controllers/userController.js
+++ src/api/controllers/userController.js
@@ -14,7 +14,7 @@ const userController = {
             const {
                 page = 1
             } = req.query
-            const limit = 5;
+            const limit = 3;
             const offset = page < 1 ? 0 : (page - 1) * limit
 
             const {
