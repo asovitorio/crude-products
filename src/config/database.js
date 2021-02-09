921957bb3e1d4ff47bb8954a60be72d50a57bd0b
Alessandro <asovitorio@gmail.com>
2021-02-08 21:10:19 -0300

    :art: teste

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,12 @@
 require('dotenv').config()
 module.exports = {
-  // username: process.env.DB_USER,
-  // password: process.env.DB_PASS,
-  // database: process.env.DB_DATABASE,
-  // host: process.env.DB_HOST,
-  // // host:'banco-dados',
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  // host:'banco-dados',
   // dialect: process.env.DB_DIALECT,
-  // port: parseInt(process.env.DB_PORT) || 3306,
+  port: parseInt(process.env.DB_PORT) || 3306,
   use_env_variable:"DATABASE_URL",
   dialect:"postgres",
   dialectOptions:{
